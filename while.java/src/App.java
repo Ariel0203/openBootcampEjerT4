0fe613ff75c5d20ffe55915ccc8cca0257dada8e
Ariel0203 <cocimanoariel@hotmail.com>
2023-01-17 14:27:43 -0300

    add While

---
+++ while.java/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numeroWhile = -3;
+        while (numeroWhile < 3){
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
+    }
+}
