d990db95c3f346e5b92450646cfb97742cef49f8
Ariel0203 <cocimanoariel@hotmail.com>
2023-01-17 13:48:05 -0300

    add If

---
+++ if.java/README.md
@@ -0,0 +1,2 @@
+Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
---
+++ if.java/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numeroIf = 5;
+        if(numeroIf > 0){
+            System.out.println ("La variable numeroIf contiene un numero Positivo");
+        }else if(numeroIf < 0){
+            System.out.println ("La variable numeroIf contiene un numero Negativo");
+        }else{
+            System.out.println ("La variable numeroIf es 0");
+        }
+    }
+}
