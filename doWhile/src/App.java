a26317ff400b209338e5f43e4cec7ca6a8965053
Ariel0203 <cocimanoariel@hotmail.com>
2023-01-23 18:37:36 -0300

    fix doWhile

--- doWhile/src/App.java
+++ doWhile/src/App.java
@@ -2,8 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         int numeroWhile = 2;
         do{
-            System.out.println(numeroWhile);
             numeroWhile++;
+            System.out.println(numeroWhile);
         }while(numeroWhile < 3);
     }
 }
